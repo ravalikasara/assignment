1fdc386653e66e1eceb069e162364558262d752b
Ravalika <ravalikasara@gmail.com>
2024-05-01 10:02:42 +0530

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3001
-app.get('/', (req, res) =>{
-  res.send('Hello World')
-  console.log("hi")
-})
-app.get('/twitter', (req, res)=> {
-    res.send('Hello World')
-    console.log("hi hi")
-  })
-app.listen(process.env.PORT,()=>{
-    console.log("app listening at ",`${port}`)
-})
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001; // Use process.env.PORT or default to 3001
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+  console.log("hi");
+});
+
+app.get('/twitter', (req, res) => {
+  res.send('Hello Twitter');
+  console.log("hi hi");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
