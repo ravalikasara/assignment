e32ab39bec9143fe0f8e30351fe6ad0577098414
Ravalika <ravalikasara@gmail.com>
2024-05-01 10:18:30 +0530

    first commit

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const app = express();
 const port = process.env.PORT || 3001; // Use process.env.PORT or default to 3001
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-  console.log("hi");
+    res.json({ message: 'Hello Twitter' });
+  
+  
 });
 
 app.get('/twitter', (req, res) => {
-  res.send('Hello Twitter');
-  console.log("hi hi");
+    res.json({ message: 'Hello Twitter' });
+  
 });
 
-app.listen(port, () => {
+
+app.listen(port, async() => {
   console.log(`App listening at http://localhost:${port}`);
+  
+a = await fetch('http://localhost:3000')
+b = await a.json();
+console.log(b)
 });
